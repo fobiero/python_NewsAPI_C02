ff1094205be87fa704025b721769e1eb9afc881e
feLycks <felix.obiero@student.moringaschool.com>
2022-02-01 01:44:18 +0300

    finish navbar build

--- run.py
+++ run.py
@@ -9,5 +9,13 @@ def home():
     return render_template('home.html')
 
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
 if __name__=="__main__":
     app.run(debug=True)
